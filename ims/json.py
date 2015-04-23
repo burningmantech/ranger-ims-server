d6e23e20528687ff40412270e93f11aa3d261da7
Wilfredo Sánchez <wsanchez@wsanchez.net>
2015-04-23 16:01:36 -0700

    Note 2013 JSON schema

--- ims/json.py
+++ ims/json.py
@@ -19,8 +19,8 @@ from __future__ import absolute_import
 """
 JSON bindings for IMS data.
 
-
-2014 JSON schema:
+2014 JSON incident schema replaces per-state time stamp attributes with a
+created time stamp plus a state attribute:
 
     {
         "number": 101,                              // int >= 0
@@ -46,6 +46,33 @@ JSON bindings for IMS data.
         "state": "closed",                          // from JSON.state_*
     }
 
+2013 JSON incident schema:
+
+    {
+        "number": 101,                              // int >= 0
+        "priority": 3,                              // int {1,3,5}
+        "summary": "Diapers, please",               // one line
+        "location_address": "8:15 & K",             // one line
+        "location_name": "Camp Fishes",             // one line
+        "ranger_handles": [
+            "Santa Cruz"                            // handle in Clubhouse
+        ],
+        "incident_types": [
+            "Law Enforcement"                       // from list in config
+        ],
+        "report_entries": [
+            {
+                "author": "Hot Yogi",               // handle in Clubhouse
+                "created": "2014-08-30T21:12:50Z",  // RFC 3339, Zulu
+                "system_entry": false,              // boolean
+                "text": "Need diapers\nPronto"      // multi-line
+            }
+        ],
+        "created": "2014-08-30T21:38:11Z"           // RFC 3339, Zulu
+        "dispatched": "2014-08-30T21:39:42Z"        // RFC 3339, Zulu
+        "on_scene": "2014-08-30T21:45:53Z"          // RFC 3339, Zulu
+        "closed": "2014-08-30T21:58:01Z"            // RFC 3339, Zulu
+    }
 """
 
 __all__ = [
