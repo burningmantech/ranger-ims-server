55e7869c81d4102ecaeb23865dcf90393dcb3120
Wilfredo Sánchez Vega <wsanchez@apple.com>
2016-04-27 13:28:57 -0700

    Add __main__ handler; prints endpoints

--- ims/service/service.py
+++ ims/service/service.py
@@ -18,6 +18,8 @@
 Incident Management System web service.
 """
 
+from __future__ import print_function
+
 __all__ = [
     "WebService",
 ]
@@ -813,3 +815,15 @@ class ContentType (Values):
 
 
 
+if __name__ == "__main__":
+    from .config import Configuration
+
+    config = Configuration(None)
+    service = WebService(config)
+
+    for rule in service.app.url_map.iter_rules():
+        methods = list(rule.methods)
+        print(
+            "{rule.rule} {methods} -> {rule.endpoint}"
+            .format(rule=rule, methods=methods)
+        )
