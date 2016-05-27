fd7cee52ab09bb39842d2271d9223e3aecbad5ed
Wilfredo Sánchez Vega <wsanchez@apple.com>
2016-05-27 13:51:36 -0700

    Also pull out external cached resource logic.

---
+++ ims/service/external.py
@@ -0,0 +1,223 @@
+##
+# See the file COPYRIGHT for copyright information.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
+"""
+Incident Management System cached external resources.
+"""
+
+__all__ = [
+    "ExternalMixIn",
+]
+
+from zipfile import BadZipfile
+
+from twisted.python.zippath import ZipArchive
+from twisted.python.url import URL
+from twisted.internet.defer import inlineCallbacks, returnValue
+from twisted.web.client import downloadPage
+
+from .http import fixedETag, HeaderName, ContentType
+from .urls import URLs
+from .klein import route
+
+
+
+class ExternalMixIn(object):
+    """
+    Mix-in for cached external resources.
+    """
+
+    bootstrapVersionNumber  = u"3.3.6"
+    jqueryVersionNumber     = u"2.2.0"
+    dataTablesVersionNumber = u"1.10.11"
+    momentVersionNumber     = u"2.13.0"
+
+    bootstrapVersion  = u"bootstrap-{}-dist".format(bootstrapVersionNumber)
+    jqueryVersion     = u"jquery-{}".format(jqueryVersionNumber)
+    dataTablesVersion = u"DataTables-{}".format(dataTablesVersionNumber)
+    momentVersion     = u"moment"
+
+    bootstrapSourceURL = URL.fromText(
+        u"https://github.com/twbs/bootstrap/releases/download/v{n}/{v}.zip"
+        .format(n=bootstrapVersionNumber, v=bootstrapVersion)
+    )
+
+    jqueryJSSourceURL = URL.fromText(
+        u"https://code.jquery.com/{v}.min.js"
+        .format(n=jqueryVersionNumber, v=jqueryVersion)
+    )
+
+    jqueryMapSourceURL = URL.fromText(
+        u"https://code.jquery.com/{v}.min.map"
+        .format(n=jqueryVersionNumber, v=jqueryVersion)
+    )
+
+    dataTablesSourceURL = URL.fromText(
+        u"https://datatables.net/releases/DataTables-{n}.zip"
+        .format(n=dataTablesVersionNumber, v=dataTablesVersion)
+    )
+
+    momentJSSourceURL = URL.fromText(
+        u"https://cdnjs.cloudflare.com/ajax/libs/moment.js/{n}/{v}.min.js"
+        .format(n=momentVersionNumber, v=momentVersion)
+    )
+
+
+    @route(URLs.bootstrapBaseURL.asText(), methods=("HEAD", "GET"), branch=True)
+    @fixedETag
+    def bootstrapResource(self, request):
+        requestURL = URL.fromText(request.uri.rstrip("/"))
+
+        # Remove URL prefix
+        names = requestURL.path[len(self.bootstrapBaseURL.path):]
+
+        request.setHeader(HeaderName.contentType.value, ContentType.CSS.value)
+        return self.cachedZippedResource(
+            request, self.bootstrapSourceURL, self.bootstrapVersion,
+            self.bootstrapVersion, *names
+        )
+
+
+    @route(URLs.jqueryJSURL.asText(), methods=("HEAD", "GET"))
+    @fixedETag
+    def jqueryJSResource(self, request):
+        request.setHeader(
+            HeaderName.contentType.value, ContentType.JavaScript.value
+        )
+        return self.cachedResource(
+            request, self.jqueryJSSourceURL,
+            "{}.min.js".format(self.jqueryVersion),
+        )
+
+
+    @route(URLs.jqueryMapURL.asText(), methods=("HEAD", "GET"))
+    @fixedETag
+    def jqueryMapResource(self, request):
+        request.setHeader(HeaderName.contentType.value, ContentType.JSON.value)
+        return self.cachedResource(
+            request, self.jqueryMapSourceURL,
+            "{}.min.map".format(self.jqueryVersion),
+        )
+
+
+    @route(
+        URLs.dataTablesBaseURL.asText(), methods=("HEAD", "GET"), branch=True
+    )
+    @fixedETag
+    def dataTablesResource(self, request):
+        requestURL = URL.fromText(request.uri.rstrip("/"))
+
+        # Remove URL prefix
+        names = requestURL.path[len(self.dataTablesBaseURL.path):]
+
+        request.setHeader(HeaderName.contentType.value, ContentType.CSS.value)
+        return self.cachedZippedResource(
+            request, self.dataTablesSourceURL, self.dataTablesVersion,
+            self.dataTablesVersion, *names
+        )
+
+
+    @route(URLs.momentJSURL.asText(), methods=("HEAD", "GET"))
+    @fixedETag
+    def momentJSResource(self, request):
+        request.setHeader(
+            HeaderName.contentType.value, ContentType.JavaScript.value
+        )
+        return self.cachedResource(
+            request, self.momentJSSourceURL,
+            "{}.min.js".format(self.momentVersion),
+        )
+
+
+    @inlineCallbacks
+    def cacheFromURL(self, url, name):
+        cacheDir = self.config.CachedResources
+
+        if not cacheDir.isdir():
+            cacheDir.createDirectory()
+
+        destination = cacheDir.child(name)
+
+        if not destination.exists():
+            tmp = destination.temporarySibling(extension=".tmp")
+            try:
+                yield downloadPage(
+                    url.asText().encode("utf-8"), tmp.open("w")
+                )
+            except:
+                self.log.failure("Download failed for {url}", url=url)
+                try:
+                    tmp.remove()
+                except (OSError, IOError):
+                    pass
+            else:
+                tmp.moveTo(destination)
+
+        returnValue(destination)
+
+
+    @inlineCallbacks
+    def cachedResource(self, request, url, name):
+        filePath = yield self.cacheFromURL(url, name)
+
+        try:
+            returnValue(filePath.getContent())
+        except (OSError, IOError) as e:
+            self.log.error(
+                "Unable to open file {filePath.path}: {error}",
+                filePath=filePath, error=e,
+            )
+            returnValue(self.notFoundResource(request))
+
+
+    @inlineCallbacks
+    def cachedZippedResource(self, request, url, archiveName, name, *names):
+        archivePath = yield self.cacheFromURL(
+            url, "{0}.zip".format(archiveName)
+        )
+
+        try:
+            filePath = ZipArchive(archivePath.path)
+        except BadZipfile as e:
+            self.log.error(
+                "Corrupt zip archive {archive.path}: {error}",
+                archive=archivePath, error=e,
+            )
+            try:
+                archivePath.remove()
+            except (OSError, IOError):
+                pass
+            returnValue(self.notFoundResource(request))
+        except (OSError, IOError) as e:
+            self.log.error(
+                "Unable to open zip archive {archive.path}: {error}",
+                archive=archivePath, error=e,
+            )
+            returnValue(self.notFoundResource(request))
+
+        filePath = filePath.child(name)
+        for name in names:
+            filePath = filePath.child(name)
+
+        try:
+            returnValue(filePath.getContent())
+        except KeyError:
+            self.log.error(
+                "File not found in ZIP archive: {filePath.path}",
+                filePath=filePath,
+                archive=archivePath,
+            )
+            returnValue(self.notFoundResource(request))
