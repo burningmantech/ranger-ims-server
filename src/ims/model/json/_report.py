24351d22d28e7e952bdeb253f9b66aa7d3f8cec8
Wilfredo Sánchez <wsanchez@wsanchez.net>
2017-07-24 16:28:14 -0700

    summary is optional

--- src/ims/model/json/_report.py
+++ src/ims/model/json/_report.py
@@ -20,7 +20,7 @@ JSON serialization/deserialization for incident reports
 
 from datetime import datetime as DateTime
 from enum import Enum, unique
-from typing import Any, Dict, List, Type
+from typing import Any, Dict, List, Optional, Type
 
 from ._json import (
     jsonDeserialize, jsonSerialize, registerDeserializer, registerSerializer
@@ -53,7 +53,7 @@ class IncidentReportJSONType(Enum):
 
     number        = int
     created       = DateTime
-    summary       = str
+    summary       = Optional[str]
     reportEntries = List[ReportEntry]
 
 
