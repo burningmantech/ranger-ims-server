50a1b70ea4333314a24c0f4290f561c5190643eb
Wilfredo Sánchez Vega <wsanchez@wsanchez.net>
2017-10-03 13:31:20 -0700

    Use new argument names when calling hypothesis.strategies.datetimes()

--- src/ims/model/strategies.py
+++ src/ims/model/strategies.py
@@ -96,10 +96,10 @@ def dateTimes(beforeNow: bool = False, fromNow: bool = False) -> DateTime:
     assert not (beforeNow and fromNow)
 
     #
-    # min_datetime >= UTC epoch because otherwise we can't store dates as UTC
+    # min_value >= UTC epoch because otherwise we can't store dates as UTC
     # timestamps.
     #
-    # We actually add a day of fuzz below because min_datetime doesn't allow
+    # We actually add a day of fuzz below because min_value doesn't allow
     # non-naive values (?!) so that ensures we have a value after the epoch
     #
     # For all current uses of model date-times in model objects in this module,
@@ -118,7 +118,7 @@ def dateTimes(beforeNow: bool = False, fromNow: bool = False) -> DateTime:
         min = DateTime(1970, 1, 1) + fuzz
 
     return _datetimes(
-        min_datetime=min, max_datetime=max, timezones=timeZones()
+        min_value=min, max_value=max, timezones=timeZones()
     )
 
 
