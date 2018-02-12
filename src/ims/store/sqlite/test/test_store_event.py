325d29ff79ec4717d3eca6fe9a11f9dc921b553e
Wilfredo Sánchez <wsanchez@wsanchez.net>
2018-02-11 20:08:04 -0800

    More event tests to superclass

--- src/ims/store/sqlite/test/test_store_event.py
+++ src/ims/store/sqlite/test/test_store_event.py
@@ -18,139 +18,20 @@
 Tests for :mod:`ranger-ims-server.store.sqlite._store`
 """
 
-from typing import Dict, Iterable, Set
-
-from hypothesis import given
-from hypothesis.strategies import frozensets, text
-
 from ims.ext.trial import TestCase
-from ims.model import Event
-from ims.model.strategies import events
 
 from .base import TestDataStore
-from ..._exceptions import StorageError
-
-Dict, Set  # silence linter
+from ...test.event import DataStoreEventTests as SuperDataStoreEventTests
 
 
 __all__ = ()
 
 
 
-class DataStoreEventTests(TestCase):
+class DataStoreEventTests(TestCase, SuperDataStoreEventTests):
     """
     Tests for :class:`DataStore` event access.
     """
 
     def store(self) -> TestDataStore:
         return TestDataStore(self)
-
-
-    def test_events(self, broken: bool = False) -> None:
-        """
-        :meth:`DataStore.events` returns all events.
-        """
-        store = self.store()
-
-        for event in (Event(id="Event A"), Event(id="Event B")):
-            store.storeEvent(event)
-
-        if broken:
-            store.bringThePain()
-
-        events = frozenset(self.successResultOf(store.events()))
-
-        self.assertEqual(events, {Event(id="Event A"), Event(id="Event B")})
-
-
-    def test_events_error(self) -> None:
-        """
-        :meth:`DataStore.events` raises `StorageError` if SQLite raises.
-        """
-        e = self.assertRaises(StorageError, self.test_events, broken=True)
-        self.assertEqual(str(e), TestDataStore.exceptionMessage)
-
-
-    @given(events())
-    def test_createEvent(self, event: Event) -> None:
-        """
-        :meth:`DataStore.createEvent` creates the given event.
-        """
-        store = self.store()
-        self.successResultOf(store.createEvent(event))
-        stored = frozenset(self.successResultOf(store.events()))
-        self.assertEqual(stored, frozenset((event,)))
-
-
-    def test_createEvent_error(self) -> None:
-        """
-        :meth:`DataStore.createEvent` raises `StorageError` if SQLite raises.
-        """
-        store = self.store()
-        store.bringThePain()
-
-        f = self.failureResultOf(store.createEvent(Event(id="x")))
-        f.printTraceback()
-        self.assertEqual(f.type, StorageError)
-
-
-    def test_createEvent_duplicate(self) -> None:
-        """
-        :meth:`DataStore.createEvent` raises :exc:`StorageError` when given an
-        event that already exists in the data store.
-        """
-        event = Event(id="foo")
-        store = self.store()
-        self.successResultOf(store.createEvent(event))
-        f = self.failureResultOf(store.createEvent(event))
-        self.assertEqual(f.type, StorageError)
-
-
-    @given(events(), frozensets(text()))
-    def test_setReaders(self, event: Event, readers: Iterable[str]) -> None:
-        """
-        :meth:`DataStore.setReaders` sets the read ACL for an event.
-        """
-        store = self.store()
-        self.successResultOf(store.createEvent(event))
-        self.successResultOf(store.setReaders(event, readers))
-        result = frozenset(self.successResultOf(store.readers(event)))
-        self.assertEqual(result, readers)
-
-
-    def test_setReaders_error(self) -> None:
-        """
-        :meth:`DataStore.setReaders` raises :exc:`StorageError` when SQLite
-        raises an exception.
-        """
-        event = Event(id="foo")
-        store = self.store()
-        self.successResultOf(store.createEvent(event))
-        store.bringThePain()
-        f = self.failureResultOf(store.setReaders(event, ()))
-        self.assertEqual(f.type, StorageError)
-
-
-    @given(events(), frozensets(text()))
-    def test_setWriters(self, event: Event, writers: Iterable[str]) -> None:
-        """
-        :meth:`DataStore.setWriters` sets the write ACL for an event.
-        """
-        store = self.store()
-        self.successResultOf(store.createEvent(event))
-        self.successResultOf(store.setWriters(event, writers))
-        result = frozenset(self.successResultOf(store.writers(event)))
-        self.assertEqual(result, writers)
-
-
-    def test_setWriters_error(self) -> None:
-        """
-        :meth:`DataStore.setWriters` raises :exc:`StorageError` when SQLite
-        raises an exception.
-        """
-        event = Event(id="foo")
-        store = self.store()
-        self.successResultOf(store.createEvent(event))
-        store.bringThePain()
-        f = self.failureResultOf(store.setWriters(event, ()))
-        self.assertEqual(f.type, StorageError)
