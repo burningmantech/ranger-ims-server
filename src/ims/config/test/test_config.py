3384038cfcc74ba732ec8da4cfaff1b2dfed381b
Wilfredo Sánchez <wsanchez@wsanchez.net>
2019-05-31 10:30:39 -0700

    Add environment variable handling. Made some config values non-optional. More tests.

--- src/ims/config/test/test_config.py
+++ src/ims/config/test/test_config.py
@@ -18,7 +18,10 @@
 Tests for L{ims.config._config}.
 """
 
+from contextlib import contextmanager
+from os import environ, getcwd
 from pathlib import Path
+from typing import Iterator, Mapping, Optional
 
 from ims.ext.trial import TestCase
 
@@ -28,8 +31,24 @@ from .._config import Configuration
 __all__ = ()
 
 
-emptyConfigFile  = Path(__file__).parent / "empty.conf"
-sampleConfigFile = Path(__file__).parent / "test.conf"
+emptyConfigFile   = Path(__file__).parent / "empty.conf"
+sampleConfigFile  = Path(__file__).parent / "test.conf"
+missingConfigFile = Path(__file__).parent / "missing.conf"
+
+
+@contextmanager
+def testingEnvironment(environment: Mapping[str, str]) -> Iterator[None]:
+    savedEnvironment = environ.copy()
+
+    environ.clear()
+    environ.update(environment)
+
+    try:
+        yield
+
+    finally:
+        environ.clear()
+        environ.update(savedEnvironment)
 
 
 
@@ -38,15 +57,11 @@ class ConfigurationTests(TestCase):
     Tests for :class:`Configuration`
     """
 
-    def test_defaults(self) -> None:
-        """
-        Check defaults.
-        """
-        assert emptyConfigFile.is_file(), emptyConfigFile
+    def _test_fromConfigFile_defaults(
+        self, configFile: Optional[Path], serverRoot: Path
+    ) -> None:
+        config = Configuration.fromConfigFile(configFile)
 
-        config = Configuration.fromConfigFile(emptyConfigFile)
-
-        serverRoot = Path(__file__).parent.parent
         configRoot = serverRoot / "conf"
         dataRoot   = serverRoot / "data"
         cached     = dataRoot / "cache"
@@ -56,18 +71,29 @@ class ConfigurationTests(TestCase):
         self.assertEquals(config.DataRoot, dataRoot)
         self.assertEquals(config.CachedResourcesRoot, cached)
 
-        self.assertEquals(config.DMSHost, None)
-        self.assertEquals(config.DMSDatabase, None)
-        self.assertEquals(config.DMSUsername, None)
-        self.assertEquals(config.DMSPassword, None)
+        self.assertEquals(config.DMSHost, "")
+        self.assertEquals(config.DMSDatabase, "")
+        self.assertEquals(config.DMSUsername, "")
+        self.assertEquals(config.DMSPassword, "")
 
 
-    def test_sampleConfig(self) -> None:
-        """
-        Check sample config.
-        """
-        assert sampleConfigFile.is_file(), sampleConfigFile
+    def test_fromConfigFile_none(self) -> None:
+        self._test_fromConfigFile_defaults(None, Path(getcwd()))
 
+
+    def test_fromConfigFile_empty(self) -> None:
+        self._test_fromConfigFile_defaults(
+            emptyConfigFile, Path(__file__).parent.parent
+        )
+
+
+    def test_fromConfigFile_missing(self) -> None:
+        self._test_fromConfigFile_defaults(
+            missingConfigFile, Path(__file__).parent.parent
+        )
+
+
+    def test_fromConfigFile_sampleConfig(self) -> None:
         config = Configuration.fromConfigFile(sampleConfigFile)
 
         serverRoot = sampleConfigFile.parent.parent
@@ -86,3 +112,45 @@ class ConfigurationTests(TestCase):
         self.assertEquals(
             config.DMSPassword, "9F29BB2B-E775-489C-9C20-9FE3EFEE1F22"
         )
+
+
+    def test_fromConfigFile_environment_value(self) -> None:
+        hostName = "xyzzy"
+
+        with testingEnvironment(dict(IMS_HOSTNAME=hostName)):
+            config = Configuration.fromConfigFile(None)
+
+        self.assertEquals(config.HostName, hostName)
+
+
+    def test_fromConfigFile_environment_path_relative(self) -> None:
+        textPath = self.mktemp()
+
+        assert not textPath.startswith("/")
+
+        with testingEnvironment(dict(IMS_SERVER_ROOT=textPath)):
+            config = Configuration.fromConfigFile(None)
+
+        self.assertTrue(Path(textPath).samefile(config.ServerRoot))
+
+
+    def test_fromConfigFile_environment_path_absolute(self) -> None:
+        path = Path(self.mktemp()).resolve()
+
+        with testingEnvironment(dict(IMS_SERVER_ROOT=str(path))):
+            config = Configuration.fromConfigFile(None)
+
+        self.assertEqual(config.ServerRoot, path)
+
+
+    def test_fromConfigFile_requireActive(self) -> None:
+        def test(value: str) -> bool:
+            with testingEnvironment(dict(IMS_REQUIRE_ACTIVE=value)):
+                config = Configuration.fromConfigFile(None)
+            return config.RequireActive
+
+        for value in ("false", "False", "FALSE", "no", "No", "NO", "0"):
+            self.assertFalse(test(value))
+
+        for value in ("true", "True", "TRUE", "yes", "Yes", "YES", "1"):
+            self.assertTrue(test(value))
