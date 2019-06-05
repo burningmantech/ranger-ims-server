1b29827f7d9f4cb9a6a268bed5def74a7603d0d4
Wilfredo Sánchez <wsanchez@wsanchez.net>
2019-06-05 13:50:13 -0700

    Add mysql-run

--- bin/.common.sh
+++ bin/.common.sh
@@ -2,6 +2,13 @@ set -eu
 
 wd="$(cd "$(dirname "$0")/.." && pwd)";
 
+    mysql_image_name="mysql:5.6";
+mysql_container_name="ranger-ims-server_mysql";
+          mysql_host="${IMS_DB_HOST_NAME:-host.docker.internal}";
+      mysql_database="${IMS_DB_DATABASE:-ims}";
+          mysql_user="${IMS_DB_USER_NAME:-ims}";
+      mysql_password="${IMS_DB_PASSWORD:-donothing}";
+
 image_repo="ranger-ims-server";
  image_tag="dev";
 image_name="${image_repo}:${image_tag}";
@@ -10,3 +17,12 @@ build_image_name="${image_repo}_build";
   ims_image_name="${image_name}";
 
 container_name="ranger-ims-server";
+
+
+mysql_port () {
+    local mapping="$(docker port "${mysql_container_name}")";
+    local netloc="${mapping##* -> }";
+    local port="${netloc##*:}";
+
+    echo "${port}";
+}
