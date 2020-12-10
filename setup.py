a6427f3470553d0abbc550259bb312c485586104
requires.io <support@requires.io>
2020-12-09 18:40:12 -0800

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -121,7 +121,7 @@ install_requirements = [
     "cffi==1.14.4",
     "characteristic==14.3.0",
     "constantly==15.1.0",
-    "cryptography==3.3",
+    "cryptography==3.3.1",
     "idna==2.10",
     "incremental==17.5.0",
     "pyasn1==0.4.8",
