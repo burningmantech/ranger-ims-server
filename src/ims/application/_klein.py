c473568ed30fa193638fb90921b6e2163f717a61
Wilfredo Sánchez <wsanchez@wsanchez.net>
2024-02-01 18:48:57 -0800

    RequestRedirect now has a new_url property

--- src/ims/application/_klein.py
+++ src/ims/application/_klein.py
@@ -377,7 +377,7 @@ class Router(Klein):
             Redirect.
             """
             assert failure.value is not None
-            url = URL.fromText(failure.value.args[0])
+            url = URL.fromText(failure.value.new_url)
             return redirect(request, url)
 
         @self.handle_errors(NotFound)
